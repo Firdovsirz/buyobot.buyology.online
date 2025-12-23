63b076555d3c40d4729e7913e5dfed4f5e5b3225
firdovsi <firdovsirz@gmail.com>
2025-12-23 17:26:08 +0400

    SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,8 @@
-import type { IConfig } from 'next-sitemap';
-
-const config: IConfig = {
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
   siteUrl: 'https://buyobot.dithari.com',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 5000,
 };
-
-export default config;
